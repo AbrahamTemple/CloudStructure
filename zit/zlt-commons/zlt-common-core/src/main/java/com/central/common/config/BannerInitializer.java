b9162621dc7c8cb46eeead0889c644ec6dae9d07
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-04-16 14:34:12 +0800

    Add files via upload

---
+++ zit/zlt-commons/zlt-common-core/src/main/java/com/central/common/config/BannerInitializer.java
@@ -0,0 +1,26 @@
+package com.central.common.config;
+
+import com.central.common.constant.CommonConstant;
+import com.central.common.utils.CustomBanner;
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.taobao.text.Color;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Banner初始化
+ *
+ * @author zlt
+ * @date
+ */
+public class BannerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        if (!(applicationContext instanceof AnnotationConfigApplicationContext)) {
+
+        }
+    }
+}
